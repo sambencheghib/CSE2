a749b54a7a9371bd0fa447feebcf44dddc596167
sambencheghib <sab219@lehigh.edu>
2015-09-15 04:27:18 +0000

    hw03

--- hw03/Block.java
+++ hw03/Block.java
@@ -19,25 +19,25 @@ public class Block {
         
 
 //Prompt the user for the length, width, and height of a block
-        System.out.print("Enter the length of the block: "); //Asking user to input length of block
+                System.out.print("Enter the length of the block: "); //Asking user to input length of block
         double lengthOfBlock = input.nextDouble(); //Accepts user input of length
-        System.out.print("Enter the width of the block: "); //Asking user to input width of block
+                System.out.print("Enter the width of the block: "); //Asking user to input width of block
         double widthOfBlock = input.nextDouble(); //Accepts user input of width
-        System.out.print("Enter the height of the block: "); //Asking user to input heigh of block
+                System.out.print("Enter the height of the block: "); //Asking user to input heigh of block
         double heightOfBlock = input.nextDouble(); //Accepts user input of height
         
 
 //Print out the output. Calculate the volum and surface area of the cube
-        double volume;
-        double surfaceArea;
+        double volume;          //Create a variable for the volume of the cube
+        double surfaceArea;     //Cretae a variable for the surface area of the cube
         
         
-        volume=(widthOfBlock*lengthOfBlock*heightOfBlock);
-        surfaceArea=2*(widthOfBlock*lengthOfBlock)+2*(lengthOfBlock*heightOfBlock)+2*(heightOfBlock*widthOfBlock);
+        volume=(widthOfBlock*lengthOfBlock*heightOfBlock);                                                              //Find the volume by multiplying width by length by height
+        surfaceArea=2*(widthOfBlock*lengthOfBlock)+2*(lengthOfBlock*heightOfBlock)+2*(heightOfBlock*widthOfBlock);      //Find the surface area by using formula 2ab + 2bc + 2ac
         
         
-        System.out.println("The volume of the block of dimensions "+lengthOfBlock+" x "+widthOfBlock+" x "+heightOfBlock+" x is "+volume+".");
-        System.out.println("The surface area of the block is "+surfaceArea+".");
+                System.out.println("The volume of the block of dimensions "+lengthOfBlock+" x "+widthOfBlock+" x "+heightOfBlock+" x is "+volume+".");  //Print statement for volume
+                System.out.println("The surface area of the block is "+surfaceArea+".");                                                                //Print statement for surface area
         
         
     } //end of main method
